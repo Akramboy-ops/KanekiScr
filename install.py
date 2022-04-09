f4c5fcee07703c12f1c53da965f39e487e17a5e5
Akramboy-ops <72228375+Akramboy-ops@users.noreply.github.com>
2022-04-10 00:40:03 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,11 @@
+import os
+
+os.system("python -m pip install --upgrade pip") 
+os.system("pip install telethon")
+os.system("pip install colorama")
+os.system("pip install requests")
+os.system("pip install python-cfonts")
+os.system("pip install get-mac")
+os.system("cls" if os.name=='nt' else 'clear')
+
+print ('All Packages Installed Sucessfully')
